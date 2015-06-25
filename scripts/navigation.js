b41667cfc00acd574fb539e0ba370c936541c27b
Biendeo <pantherman1996@gmail.com>
2015-06-25 18:49:35 +1000

    I might've changed the JS so that it dynamically knows where the website is hosted, and will set the base URL on that. Dynamic website hosting might be a full thing now. :)

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,6 +1,18 @@
 ﻿console.log("The header is a magical javascript file. Wooooh!");
+
+// This gets the computer's URL and port number.
+// This will be used to get the data from wherever the website is hosted.
+var url = window.location.hostname;
+var port = window.location.port;
+
+// If you're looking at this, then this is just for debugging.
+// Purely, if I'm testing this locally, I want all the web elements to come from my computer.
+if (url == "localhost") {
+	url = "http://" + url + ":" + port + "/";
+}
+
 document.write(" \
-<base href=\"https://biendeo.github.io\"> \
+<base href=" + url + "> \
 <link href=\"main.css\" rel=\"stylesheet\"> \
 <link rel=\"icon\" type=\"image/png\" href=\"images/favicon.png\"> \
 <div class=\"navigation\"> \
