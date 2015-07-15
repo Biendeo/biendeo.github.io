7fa0ba0491395c6975046a16bc92172e4ddb082f
Biendeo <pantherman1996@gmail.com>
2015-07-15 21:11:59 +1000

    Added a system so that the navigation bar is now its own HTML element, and is added by the page. I also need to separate the base URL bit. Now that's its own file. Hopefully this should lead to some easier edits. Next commit will incorporate this to every page.

---
+++ scripts/baseurlgetter.js
@@ -0,0 +1,13 @@
+﻿// This gets the computer's URL and port number.
+// This will be used to get the data from wherever the website is hosted.
+var url = window.location.hostname;
+var port = window.location.port;
+
+// If you're looking at this, then this is just for debugging.
+// Purely, if I'm testing this locally, I want all the web elements to come from my computer.
+if (url == "localhost") {
+	url = "http://" + url + ":" + port + "/";
+} else {
+	url = "https://" + url + "/";
+}
+document.write("<base href=" + url + ">");
