f7428b0b33726854ccdb721cdf5ada7d6e678355
Biendeo <pantherman1996@gmail.com>
2015-07-16 09:56:37 +1000

    Made it so that the main page now loads the about content into the same page rather than loading a new page. Will it be better? Maybe, depends. The old system shouldn't be too hard to re-implement. Just revert the changes.

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,3 +1,11 @@
 ﻿$(document).ready(function () {
 	$(".navigation").load("pages/elements/navigation.html");
 });
+
+var loadIndex = function () {
+	$(".mainarea").load("pages/elements/indexcontent.html");
+};
+
+var loadAbout = function () {
+	$(".mainarea").load("pages/elements/aboutcontent.html");
+};
