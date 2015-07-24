fd6d140e67a54578fe5382ce3cfdf82acea61130
Biendeo <pantherman1996@gmail.com>
2015-07-25 08:29:38 +1000

    I changed a bit so that now the website changes the URL automatically. This should allow the user to go forward and back between the pages too.

--- scripts/baseurlgetter.js
+++ scripts/baseurlgetter.js
@@ -4,14 +4,14 @@ var protocol = window.location.protocol
 var url = window.location.hostname;
 var port = window.location.port;
 
-console.log(protocol + url + port);
-
 // If you're looking at this, then this is just for debugging.
 // Purely, if I'm testing this locally, I want all the web elements to come from my computer.
 if (url == "localhost") {
     url = "http://" + url + ":" + port + "/";
 } else {
     if (protocol == "http:") {
+        // Chrome did a weird thing in the recent update, so now the site should
+        // automatically redirect to the HTTPS version.
         window.location.replace("https://" + url + "/");
     }
     url = "https://" + url + "/";
