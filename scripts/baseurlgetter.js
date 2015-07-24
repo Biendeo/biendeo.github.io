862e006f543fc8db01924fd8f355852ff9454ea7
Biendeo <pantherman1996@gmail.com>
2015-07-25 07:20:39 +1000

    I think Chrome broke HTTPS redirection, so I made my own. Hopefully, if the website is hosted on HTTPS, it'll automatically redirect to that version. I believe the JQuery bits were only broken, so that should mean this runs. Let's see.
    Also, set up Visual Studio.

--- scripts/baseurlgetter.js
+++ scripts/baseurlgetter.js
@@ -1,13 +1,19 @@
 ﻿// This gets the computer's URL and port number.
 // This will be used to get the data from wherever the website is hosted.
+var protocol = window.location.protocol
 var url = window.location.hostname;
 var port = window.location.port;
+
+console.log(protocol + url + port);
 
 // If you're looking at this, then this is just for debugging.
 // Purely, if I'm testing this locally, I want all the web elements to come from my computer.
 if (url == "localhost") {
-	url = "http://" + url + ":" + port + "/";
+    url = "http://" + url + ":" + port + "/";
 } else {
-	url = "https://" + url + "/";
+    if (protocol == "http:") {
+        window.location.replace("https://" + url + "/");
+    }
+    url = "https://" + url + "/";
 }
 document.write("<base href=" + url + ">");
