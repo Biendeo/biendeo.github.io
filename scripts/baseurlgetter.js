49eed99706cbcdeb0f3bb52137378e11b8d9689b
Biendeo <pantherman1996@hotmail.com>
2015-07-31 20:38:12 +1000

    Added a bit to the base URL getter so that it now works if the website is hosted as a subdomain of another website (like my uni hosting). It's also a bit of regexp testing. :)

--- scripts/baseurlgetter.js
+++ scripts/baseurlgetter.js
@@ -2,18 +2,24 @@
 // This will be used to get the data from wherever the website is hosted.
 var protocol = window.location.protocol
 var url = window.location.hostname;
+var path = window.location.pathname;
 var port = window.location.port;
+
+// Now we clean the pathname of folders that I'm expecting.
+// This allows me to host this as a subdomain of another website.
+path = path.replace(path.substring(/\/pages|\/scripts|\/images|\/.*\.html/g), "");
 
 // If you're looking at this, then this is just for debugging.
 // Purely, if I'm testing this locally, I want all the web elements to come from my computer.
 if (url == "localhost") {
-    url = "http://" + url + ":" + port + "/";
+    url = "http://" + url + ":" + port + path;
 } else {
     if (protocol == "http:") {
         // Chrome did a weird thing in the recent update, so now the site should
         // automatically redirect to the HTTPS version.
-        window.location.replace("https://" + url + "/");
+        window.location.replace("https://" + url + path);
     }
     url = "https://" + url + "/";
 }
+
 document.write("<base href=" + url + ">");
